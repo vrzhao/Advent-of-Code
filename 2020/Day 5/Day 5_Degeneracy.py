9a3371a0baf3d2059116a870270ecea32c4d39b5
vrzhao <coldfusion000@gmail.com>
2020-12-05 04:04:53 -0500

    Added a non-regex version
    no longer need to import re, true oneline solution

--- 2020/Day 5/Day 5_Degeneracy.py
+++ 2020/Day 5/Day 5_Degeneracy.py
@@ -15,3 +15,9 @@ print("Part 1: ", max([int(re.sub('[BR]','1',re.sub('[FL]','0',row.strip('\n')))
 end = time.time()
 print(end-start)
 
+start = time.time()
+
+print("Part 1: ", max([int(row.strip('\n').replace('F','0').replace('B','1').replace('L','0').replace('R','1'),2) for row in open("input.txt", "r")]), "\t Part 2: ", max([seatID+1 for seatID in [int(row.strip('\n').replace('F','0').replace('B','1').replace('L','0').replace('R','1'),2) for row in open("input.txt", "r")] if (seatID+2 in [int(row.strip('\n').replace('F','0').replace('B','1').replace('L','0').replace('R','1'),2) for row in open("input.txt", "r")] and seatID+1 not in [int(row.strip('\n').replace('F','0').replace('B','1').replace('L','0').replace('R','1'),2) for row in open("input.txt", "r")])]))
+
+end = time.time()
+print(end-start)
