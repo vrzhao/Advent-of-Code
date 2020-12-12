fe666a5b73257bec96b6ac911a34fa42082dcb44
vrzhao <coldfusion000@gmail.com>
2020-12-12 06:19:53 -0500

    Scope changes
    made scope changes for clarity and included file IO for additional test data

--- 2020/Day 11/Day 11.py
+++ 2020/Day 11/Day 11.py
@@ -11,6 +11,8 @@ import copy
 layout = []
 
 with open("input.txt", "r") as f:
+#with open("test.txt", "r") as f:    
+#2418 for part 1 and 2144 for part 2 
     for row in f:
         seats = []
         row = row.strip('\n')
@@ -20,17 +22,17 @@ with open("input.txt", "r") as f:
 
 max_X = len(layout[0])
 max_Y = len(layout)
-#print(max_X*max_Y)
-def check_seat(x,y):
+
+def check_seat(x,y,layout):
     try:
-        if new_layout[y][x] == '#':
+        if layout[y][x] == '#':
             return 1
         else:
             return 0
     except IndexError:
         return 0
 
-def check_adjacent(x, y):
+def check_adjacent(x, y,layout):
     list_X = [x-1,x,x+1]
     list_Y = [y-1,y,y+1]
     
@@ -40,16 +42,14 @@ def check_adjacent(x, y):
         for y1 in list_Y:
             if (x1 == x and y1 == y):
                 continue
-            occupied += check_seat(x1,y1)
+            occupied += check_seat(x1,y1,layout)
     return occupied
 
 start = time.time()
 
 new_layout = copy.deepcopy(layout)
-run = 1
+run = 0
 
-#def change_seats(layout):
-#    pass
 def occupied_seats(layout):
     occupied = 0
 
@@ -65,19 +65,21 @@ while(True):
     for y in range(0,max_Y):
         for x in range(0,max_X):
 #            print(x,y)
-            adjacency = check_adjacent(x,y)
+            adjacency = check_adjacent(x,y,new_layout)
             if new_layout[y][x] == 'L' and adjacency == 0:
                 temp_layout[y][x] = '#'
                 changes += 1
             elif new_layout[y][x] == '#' and adjacency >= 4:
                 temp_layout[y][x] = 'L'
                 changes += 1
+            elif new_layout[y][x] == '.':
+                continue
     if(changes == 0):
         break
     new_layout = copy.deepcopy(temp_layout)
     run += 1
 
-    print(run,occupied_seats(new_layout))
+    print(run,occupied_seats(temp_layout))
 
 
 end = time.time()
